3ca2afd2b4db3731e3c5f7f8e6162a72b1392854
kearnz <josephkearney14@gmail.com>
2018-05-15 21:52:58 -0400

    commander for cli

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ global.fetch = require("node-fetch");
 // get ref data and pricing methods
 const refdata = require("./calls/refdata"),
     pricing = require("./calls/pricing"),
+    command = require("commander"),
     cc = require("cryptocompare");
 
-/* example use of functions 
 // creates the generic csv's
 refdata.getCoins("coins.csv");
 refdata.getExchanges("exchanges.csv");
 // generate daily and hourly for top 5 coins
-["BTC","ETH","BCH","XRP","EOS"].map(i => {
-    pricing.getPrices(cc.histoDay,i,"USD",{"limit":2000})
-    pricing.getPrices(cc.histoHour,i,"USD",{"limit":2000})
-});
-*/
+//["BTC","ETH","BCH","XRP","EOS"].map(i => {
+//    pricing.getPrices(cc.histoDay,i,"USD",{"limit":2000})
+//    pricing.getPrices(cc.histoHour,i,"USD",{"limit":2000})
+//});
+
